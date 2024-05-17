e415fe33f2054f210f31d83717c9d141d7be3b1d
vminkmar <vminkmar@student.42heilbronn.de>
2024-05-17 19:58:30 +0200

    changes home.html to base.html and test.html to home.html

--- src/backend/api/views_content.py
+++ src/backend/api/views_content.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 
 
 def homeView(request):
-	return render(request, 'home.html')
+	return render(request, 'base.html')
 
 def loginFormView(request):
 	return render(request, 'loginForm.html')
 
 def testView(request):
-	return render(request, 'test.html')
+	return render(request, 'home.html')
 
 def twoFAView(request):
 	return render(request, '2FA.html')
