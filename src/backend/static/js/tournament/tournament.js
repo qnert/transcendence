dcb2fba9bc215fcc7a61ce278492738b8db8ae63
jweeeezy <willertjakob@gmail.com>
2024-06-07 20:10:06 +0200

    add actual create_tournament functionality for endpoint

--- src/backend/static/js/tournament/tournament.js
+++ src/backend/static/js/tournament/tournament.js
@@ -1,12 +1,9 @@
-const URL = "/create-tournament/";
 async function createTournament() {
-    console.log("Creating Tournament");
-
+    const URL = "/create-tournament/";
     const promise = await fetch(URL, {
         method: "POST",
-        body: JSON.stringify({user: "some_username", tournament_name: "tournament1"}),
+        body: JSON.stringify({tournament_name: "tournament1"}),
     });
-    console.log(promise);
     const processedResponse = await promise.json();
     console.log(processedResponse);
 }
--- src/backend/tournament/views.py
+++ src/backend/tournament/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt  # disable token security for testing
+from django.views.decorators.csrf import csrf_exempt  # @note disable token security for testing
+from tournament.models import Tournament
+from api.models import UserProfile
+import json
 
 
 def tournament(request):
     return render(request, 'tournament.html')
 
 
-@csrf_exempt  # @note just for testing purposes
+@csrf_exempt  # @note
 def create_tournament(request):
-    if request.method == "POST":
-        print("correct request")
-    return JsonResponse({"message": "received something"}, status=201)
+    request_json = json.loads(request.body)
+    user_profile = UserProfile.objects.get(user=request.user)
+    tournament_name = request_json.get("tournament_name")
+    print(f"{user_profile}")
+    print(f"{tournament_name}")
+
+    Tournament.objects.create(name=tournament_name, created_by=user_profile)
+    return JsonResponse({"message": "success"}, status=201)
