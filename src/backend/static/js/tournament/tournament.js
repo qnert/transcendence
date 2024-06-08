9d22b7b11d0f0fc09ced6aa3af6d4b8ee56c5254
jweeeezy <willertjakob@gmail.com>
2024-06-08 12:52:20 +0200

    implement createTournament form functionality, add input parsing

--- src/backend/static/js/tournament/tournament.js
+++ src/backend/static/js/tournament/tournament.js
@@ -1,9 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("tournament-form");
+    const tournamentNameField = document.getElementById(
+        "tournament-form-name-field"
+    );
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        const tournamentName = tournamentNameField.value;
+        if (/^(?=.*[a-zA-Z])[a-zA-Z0-9_-]+$/.test(tournamentName)) {
+            createTournament(tournamentName);
+        } else {
+            // @note might change this to better UX
+            alert(
+                "Invalid tournament name. Only a-z A-Z 0-9 _ - are allowed, and it must contain at least one letter."
+            );
+        }
+    });
+});
 
-async function createTournament() {
+async function createTournament(tournamentName) {
     const URL = "/create-tournament/";
     const promise = await fetch(URL, {
         method: "POST",
-        body: JSON.stringify({ tournament_name: "tournament1" }),
+        body: JSON.stringify({ tournament_name: tournamentName }),
     });
     const processedResponse = await promise.json();
 
