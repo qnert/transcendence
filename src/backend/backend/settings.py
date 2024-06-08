dbb692cb999589cf7da21665b9379eb831cd2ca6
vminkmar <vminkmar@student.42heilbronn.de>
2024-06-08 15:26:31 +0200

    adds migrations

--- src/backend/backend/settings.py
+++ src/backend/backend/settings.py
@@ -153,16 +153,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    BASE_DIR / "src/backend/static",
+    os.path.join(BASE_DIR, "static"),
 ]
+
 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+LOGIN_URL = "0.0.0.0:8000/login"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
