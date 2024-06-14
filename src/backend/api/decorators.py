bede036d863b5c726e57743429a89a4fc403a4dd
vminkmar <vminkmar@student.42heilbronn.de>
2024-06-14 13:08:57 +0200

    changes decorator status codes

--- src/backend/api/decorators.py
+++ src/backend/api/decorators.py
@@ -13,19 +13,19 @@ def own_login_required(view_func):
                 return render(request, 'base.html')
         elif not request.user.is_logged_in:
             if request.headers.get('Accept') == 'application/json':
-                return JsonResponse({'error': 'You are not logged in!'}, status=403)
+                return JsonResponse({'error': 'You are not logged in!'}, status=401)
             else:
                 return render(request, 'base.html')
         return view_func(request, *args, **kwargs)
     return _wrapped_view
 
 
-def OTP_required(view_func):
+def twoFA_required(view_func):
 	def _wrapped_view(request, *args, **kwargs):
 		if(request.user.is_2fa_enabled):
 			if isinstance(request.user, AnonymousUser):
-				return JsonResponse({'error': '2FA required'}, status=597)
+				return JsonResponse({'error': '2FA required'}, status=401)
 			if not getattr(request.user, 'completed_2fa', False):
-				return JsonResponse({'error': '2FA required'}, status=598)
+				return JsonResponse({'error': '2FA required'}, status=401)
 			return view_func(request, *args, **kwargs)
 	return _wrapped_view
