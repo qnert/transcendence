505f73d87cef7f1e6ef78deabb70174ff4c4ad62
vminkmar <vminkmar@student.42heilbronn.de>
2024-06-14 14:24:10 +0200

    changes settings.py

--- src/backend/backend/settings.py
+++ src/backend/backend/settings.py
@@ -70,7 +70,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-	'backend.middleware.jwt_middleware'
 ]
 
 
