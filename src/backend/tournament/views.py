8fbd181d9683dd3d79840deb9c7dab11fb66ea42
jweeeezy <willertjakob@gmail.com>
2024-06-16 18:49:02 +0200

    add tourn view

--- src/backend/tournament/views.py
+++ src/backend/tournament/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-# from django.core.exceptions import ObjectDoesNotExist
-from django.views.decorators.csrf import csrf_exempt  # @note disable token security for testing
+from django.views.decorators.csrf import csrf_exempt  # TODO remove
 from tournament.models import Tournament
 from api.models import UserProfile
 import json
@@ -18,15 +17,15 @@ def tournament_hub(request):
         return render(request, 'tournament_hub.html', {'tournaments': tournaments})
 
 
+def tournament_lobby(request, lobby_name):
+    if (request.method == "GET"):
+        return render(request, "tournament_lobby.html", {"lobby_name": lobby_name})
+
+
 def tournament_api_get_list(request):
     if (request.method == "GET"):
         tournaments = list(Tournament.objects.all().values())
         return render(request, 'tournament_list.html', {'tag': 'option', 'tournaments': tournaments})
-
-
-def tournament_lobby(request, lobby_name):
-    if (request.method == "GET"):
-        return render(request, "tournament_lobby.html", {"lobby_name": lobby_name})
 
 
 @csrf_exempt
@@ -36,31 +35,7 @@ def tournament_api_create(request):
         user_profile = UserProfile.objects.get(user=request.user)
         if tournament_name is not None:
             if Tournament.objects.filter(name=tournament_name).exists():
-                return JsonResponse({"error": "Tournament exists already!"}, status=400)
+                return JsonResponse({"error": "Tournament exists already!"}, status=409)
 
             Tournament.objects.create(name=tournament_name, created_by=user_profile)
             return JsonResponse({"message": "Success"}, status=201)
-
-
-# @csrf_exempt
-# TODO check if error handling is actually required because parsing is mostly done in frontend already
-# def create_tournament(request):
-#    # TODO check for POST request
-#    try:
-#        request_json = json.loads(request.body)
-#        user_profile = UserProfile.objects.get(user=request.user)
-#    except ObjectDoesNotExist:
-#        return JsonResponse({"error": "UserProfile doesn't exist for the current user"}, status=400)
-#    except json.JSONDecodeError:
-#        return JsonResponse({"error": "Invalid JSON data in request body"}, status=400)
-#
-#    tournament_name = request_json.get("tournament_name")
-#    if not tournament_name:
-#        return JsonResponse({"error": "Tournament name is required"}, status=400)
-#
-#    if Tournament.objects.filter(name=tournament_name).exists():
-#        return JsonResponse({"error": "Tournament name is already taken"}, status=409)
-#
-#    Tournament.objects.create(name=tournament_name, created_by=user_profile)
-#
-#    return JsonResponse({"message": "success"}, status=201)
