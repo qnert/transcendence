6c8c763d7f96c0213b3aa2f80432388ccd2ca56b
vminkmar <vminkmar@student.42heilbronn.de>
2024-06-18 17:43:11 +0200

    updates fetchProfileData() and adds 401 status code

--- src/backend/static/js/profile/fetch_profile.js
+++ src/backend/static/js/profile/fetch_profile.js
@@ -1,3 +1,7 @@
+import { handle401Error } from "../basics.js";
+
+
+
 export function fetchProfileData() {
     const token = localStorage.getItem("access_token");
     fetch("/api/get_profile/", {
@@ -7,31 +11,34 @@ export function fetchProfileData() {
             Authorization: `Bearer ${token}`,
         },
     })
-        .then((response) => {
-            console.log("Raw Response Status:", response.status);
-            return response.text();
-        })
-        .then((rawData) => {
-            try {
-                const data = JSON.parse(rawData);
-                if (data) {
-                    document.getElementById("profile-name").textContent = data.username;
-                    document.getElementById("profile-username").value = data.username;
-                    document.getElementById("profile-email").value = data.email;
-                    document.getElementById("profile-display_name").value = data.display_name;
-                    document.getElementById("profile-picture_url").value = data.picture_url;
-                    const profilePictureUrl = data.profile_picture;
-                    if (profilePictureUrl) {
-                        console.log(profilePictureUrl);
-                        const profilePicture = document.getElementById("profile-picture");
-                        profilePicture.setAttribute("src", profilePictureUrl);
-                    }
-                }
-            } catch (error) {
-                console.error("Error parsing JSON:", error);
+	.then((response) => {
+		if (!response.ok) {
+			if (response.status == 401) {
+				handle401Error();
+				return;
+			}
+			if (response.status == 405) {
+				console.log("Method not allowed");
+				return;
+			}
+		}
+		return response.json();
+	})
+    .then((data) => {
+        if (data) {
+            document.getElementById("profile-name").textContent = data.username;
+            document.getElementById("profile-username").value = data.username;
+            document.getElementById("profile-email").value = data.email;
+            document.getElementById("profile-display_name").value = data.display_name;
+            document.getElementById("profile-picture_url").value = data.picture_url;
+            const profilePictureUrl = data.profile_picture;
+            if (profilePictureUrl) {
+                const profilePicture = document.getElementById("profile-picture");
+                profilePicture.setAttribute("src", profilePictureUrl);
             }
-        })
-        .catch((error) => {
-            console.error("Error fetching profile data:", error);
-        });
+        }
+    })
+    .catch((error) => {
+        console.error("Error fetching profile data:", error);
+    });
 }
