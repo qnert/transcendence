e21907a556cf8316786e6e31fb32aeec02502c6b
vminkmar <vminkmar@student.42heilbronn.de>
2024-06-18 14:37:40 +0200

    adds access and refresh token to database

--- src/backend/api/models.py
+++ src/backend/api/models.py
@@ -13,6 +13,8 @@ class User(AbstractUser):
 	last_name = models.CharField(max_length=255)
 	email = models.CharField(max_length=255, unique=True)
 	password = models.CharField(max_length=255)
+	access_token = models.CharField(max_length=255, unique=True, null=True)
+	refresh_token = models.CharField(max_length=255, unique=True, null=True)
 	username = models.CharField(max_length=255, unique=True)
 	is_2fa_enabled = models.BooleanField(default=False)
 	completed_2fa = models.BooleanField(default=False)
