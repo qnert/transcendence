36710e6b4dcbd8a6bb64a0f1d99981939bb31217
vminkmar <vminkmar@student.42heilbronn.de>
2024-06-21 14:07:43 +0200

    adds friend page

--- src/backend/api/urls_content.py
+++ src/backend/api/urls_content.py
@@ -8,5 +8,6 @@ urlpatterns = [
 	path('2FA/', views_content.twoFAView),
 	path('set_passwd/', views_content.set_passwd, name="set_passwd"),
 	path('profile/', views_content.profileView),
+	path('friend/<str:name>/', views_content.friends_profile),
 	path('chat/', views_content.chatView),
 ]
