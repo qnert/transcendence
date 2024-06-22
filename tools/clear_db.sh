72efbff865e878c22d6e4e3a17f8ee1be5203063
jweeeezy <willertjakob@gmail.com>
2024-06-22 13:08:18 +0200

    [tools/clear.db] remove sudo for macos usage

--- tools/clear_db.sh
+++ tools/clear_db.sh
@@ -1,3 +1,3 @@
-sudo rm -rf src/PostgreSQL/
-sudo find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-sudo find . -path "*/migrations/*.pyc"  -delete
+rm -rf src/PostgreSQL/
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc"  -delete
