68c5faa756d8c6a783a87d4eb7c45b06b2a1a5ba
vminkmar <vminkmar@student.42heilbronn.de>
2024-06-23 17:14:35 +0200

    adds button for match history and function to get the history

--- src/backend/static/js/profile/buttons.js
+++ src/backend/static/js/profile/buttons.js
@@ -2,6 +2,7 @@ import { handle401Error, handleRoute, updateContentToken } from "../basics.js";
 import { fetchProfileData } from "./fetch_profile.js";
 import { getCookie } from "../security/csrft.js";
 import { checkBox } from "./2FA.js";
+import { getAccessToken } from "../security/jwt.js";
 
 export async function saveChanges() {
     const picture_url = document.getElementById("profile-picture_url").value;
@@ -56,5 +57,55 @@ export async function bindProfileButton() {
             await handleRoute(event, "/profile/");
             await fetchProfileData();
         };
+    }
+}
+
+export async function matchHistoryButton(){
+	const matchHistory = document.getElementById("matchHistory");
+	if(matchHistory){
+		matchHistory.onclick = async function(event){
+			event.preventDefault();
+			await handleRoute(event, "/history/");
+			await getGameHistory();
+		}
+	}
+}
+
+export async function getGameHistory(){
+	const token = localStorage.getItem("access_token");
+	try {
+        const response = await fetch("/game_history/", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        if (!response.ok) {
+			if(response.status === 401){
+				handle401Error();
+				return;
+			}
+            const errorData = await response.json();
+            alert(errorData.error);
+        } else {
+			const data = await response.json();
+            const historyTable = document.getElementById('history-body');
+            historyTable.innerHTML = '';
+
+            data.forEach(result => {
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                    <td>${result.opponent_profile}</td>
+                    <td>${result.user_score}</td>
+                    <td>${result.opponent_score}</td>
+                    <td>${result.is_win ? 'Win' : 'Loss'}</td>
+                    <td>${result.date_played}</td>
+                `;
+                historyTable.appendChild(row);
+            });
+        }
+    } catch (error) {
+        console.error("Something went wrong:", error);
     }
 }
