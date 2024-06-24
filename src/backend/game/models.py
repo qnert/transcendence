a8e3c06dc8aca0400766e4d015d1b5ad57b277e1
vminkmar <vminkmar@student.42heilbronn.de>
2024-06-24 14:33:26 +0200

    adds null=True to all variables

--- src/backend/game/models.py
+++ src/backend/game/models.py
@@ -6,7 +6,10 @@ from django.utils import timezone
 class GameResult(models.Model):
   user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='game_results')
   opponent_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='game_results_opp')
-  user_score = models.IntegerField()
-  opponent_score = models.IntegerField()
-  is_win = models.BooleanField()
+  user_score = models.IntegerField(null=True)
+  opponent_score = models.IntegerField(null=True)
+  is_win = models.BooleanField(null=True)
+  max_rally = models.IntegerField(null=True)
+  min_rally = models.IntegerField(null=True)
+  average_rally = models.FloatField(null=True)
   date_played = models.DateTimeField(default=timezone.now)
