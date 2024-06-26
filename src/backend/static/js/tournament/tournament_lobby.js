5ac73259c6fff73ef8e9adc7eb130e95fd1571bf
jweeeezy <willertjakob@gmail.com>
2024-06-26 18:22:31 +0200

    format tournament_lobby.js, remove unnecessary code

--- src/backend/static/js/tournament/tournament_lobby.js
+++ src/backend/static/js/tournament/tournament_lobby.js
@@ -4,57 +4,48 @@ let tournamentLobbySocket;
 
 // =========================== MAIN EVENT LOOP ===============================
 
-// TODO might remove
-// TODO event listener either gobal or here if url changes
-export async function tournamentLobbyEventLoop() {}
-
 export function tournamentLobbyInit(lobbyName, userName) {
-  const tournamentLobbyChatLog = document.getElementById("lobby-chat-log");
-  const tournamentLobbyChatInput = document.getElementById("lobby-chat-message-input");
-  const tournamentLobbyChatSubmit = document.getElementById("lobby-chat-message-submit");
-  tournamentLobbySocket = new WebSocket(
-    "ws://" + window.location.host + "/ws/tournament/lobby/" + lobbyName + "/" + userName + "/"
-  );
+    // this code is not run in reattachEventListener, so protection shouldnt be needed
+    const tournamentLobbyChatLog = document.getElementById("lobby-chat-log");
+    const tournamentLobbyChatInput = document.getElementById("lobby-chat-message-input");
+    const tournamentLobbyChatSubmit = document.getElementById("lobby-chat-message-submit");
+    tournamentLobbySocket = new WebSocket("ws://" + window.location.host + "/ws/tournament/lobby/" + lobbyName + "/" + userName + "/");
 
-  tournamentLobbySocket.onmessage = function (event) {
-    const data = JSON.parse(event.data);
-    let message = "";
-    if (data.username) {
-      message = `${data.username}: ${data.message}`;
-    } else {
-      message = `${data.message}`;
-    }
-    tournamentLobbyChatLog.value += message + "\n";
-  };
+    tournamentLobbySocket.onmessage = function (event) {
+        const data = JSON.parse(event.data);
+        let message = "";
+        if (data.username) {
+            message = `${data.username}: ${data.message}`;
+        } else {
+            message = `${data.message}`;
+        }
+        tournamentLobbyChatLog.value += message + "\n";
+    };
 
-  tournamentLobbySocket.onclose = function () {
-    console.error("Chat socket closed unexpectedly");
-    // TODO leave tournament
-  };
+    tournamentLobbyChatInput.focus();
+    tournamentLobbyChatInput.onkeyup = function (event) {
+        if (event.key === "Enter") {
+            tournamentLobbyChatSubmit.click();
+        }
+    };
 
-  tournamentLobbyChatInput.focus();
-  tournamentLobbyChatInput.onkeyup = function (event) {
-    if (event.key === "Enter") {
-      tournamentLobbyChatSubmit.click();
-    }
-  };
-  tournamentLobbyChatSubmit.onclick = function () {
-    const message = tournamentLobbyChatInput.value;
-    tournamentLobbySocket.send(
-      JSON.stringify({
-        message: message,
-      })
-    );
-    tournamentLobbyChatInput.value = "";
-  };
+    tournamentLobbyChatSubmit.onclick = function () {
+        const message = tournamentLobbyChatInput.value;
+        tournamentLobbySocket.send(
+            JSON.stringify({
+                message: message,
+            })
+        );
+        tournamentLobbyChatInput.value = "";
+    };
 }
 
 // =========================== CLEAN UP ===============================
 
 export function tournamentLobbyCloseSocket() {
-  if (tournamentLobbySocket) {
-    console.log("closing lobby socket");
-    tournamentLobbySocket.close();
-    tournamentLobbySocket = null;
-  }
+    if (tournamentLobbySocket) {
+        console.log("[DEBUG] closing tournamentLobbySocket");
+        tournamentLobbySocket.close();
+        tournamentLobbySocket = null;
+    }
 }
